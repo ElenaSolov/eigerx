cd73478c05ed09e0fc0d7d15211d1f63f3565e83
Elena Soloveva <seaver@yandex.ru>
2023-01-12 12:04:55 +0200

    Feat: add getData on button click and autoRefresh functionality

--- pricewidget/src/components/priceWidget/priceWidget.js
+++ pricewidget/src/components/priceWidget/priceWidget.js
@@ -1,37 +1,63 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import priceWidgetStyles from "./priceWidget.module.css";
-import { initialMessage } from "../../constants";
+import { initialMessage } from "../../utils/constants";
+import { getPrices } from "../../utils/api";
 
 const PriceWidget = () => {
-  const [load, setLoad] = useState(false);
-  //   useEffect(()=>{
-  // //   getPrices()
-  //   })
+  const [data, setData] = useState(null);
+  const [auto, setAuto] = useState(false);
+
+  const getData = () => {
+    console.log("Get request sent"); //this is left for testing purposes
+    getPrices()
+      .then((res) => setData(res.result))
+      .catch((err) => console.log(err)); //should be more than console.log, of course
+  };
+
+  useEffect(() => {
+    //in case of fetching data every 3 seconds web sockets are more appropriate solution
+    let interval;
+    if (auto) {
+      getData();
+      interval = setInterval(getData, 3000);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  });
+
+  const handleChange = () => {
+    setAuto((prev) => !prev);
+  };
+
   return (
     <section>
-      {load ? (
+      {data ? (
         <ul className={priceWidgetStyles.list}>
-          <li>
-            <p>Asset</p>
-          </li>
-          <li>
-            <p>Price</p>
-          </li>
-          <li>
-            <p></p>
-          </li>
-          <li>
-            <p></p>
-          </li>
+          <li className={priceWidgetStyles.item}>Asset</li>
+          <li className={priceWidgetStyles.item}>Price</li>
+          {data.map((item, index) => (
+            <React.Fragment key={index}>
+              <li className={priceWidgetStyles.item}>{item.asset}</li>
+              <li className={priceWidgetStyles.item}>{item.price}</li>
+            </React.Fragment>
+          ))}
         </ul>
       ) : (
-        <p>{initialMessage}</p>
+        <p className={priceWidgetStyles.text}>{initialMessage}</p>
       )}
-      <div>
-        <button>Refresh</button>
-        <label htmlFor="auto">
+      <div className={priceWidgetStyles.buttonBar}>
+        <button className={priceWidgetStyles.refreshBtn} onClick={getData}>
+          Refresh
+        </button>
+        <label className={priceWidgetStyles.label} htmlFor="auto">
           Auto-Refresh
-          <input type="checkbox" id="auto" />
+          <input
+            type="checkbox"
+            id="auto"
+            checked={auto}
+            onChange={handleChange}
+          />
         </label>
       </div>
     </section>
